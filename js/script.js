06b5b79b085f027c8cd3575bcfe61b05e4e73202
LucaFaedda <lucakarl91@gmail.com>
2022-12-06 17:05:34 +0100

    cambiato il for

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 
+
+
 function generateSquare()
 {
     const quadrato = document.createElement('div') // creo un div 
     quadrato.classList.add('square') // al div creato do la classe square
     return quadrato
-
 }
 
-const grid = document.getElementById('grind') // richiamo da html grid
 
-let number;
+const play = document.getElementById('chek-me')
 
-for (let i =0 ; i < 100 ; i++){
-
-    number = i
-    console.log(number)
-    const box = generateSquare() // qua mi sono creato i vari square
-    console.log(box)
-    grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
-
-}
-
+play.addEventListener('click', function(){
+    const grid = document.getElementById('grind') // richiamo da html grid
+    
+    let number;
+    
+    for (let i =1 ; i <= 100 ; i++){
+    
+        number = i
+        const box = generateSquare() // qua mi sono creato i vari square
+        grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+        box.innerHTML += number
+        box.addEventListener('click', function(){
+            box.classList.add('yellow')
+            console.log(number)
+        })
+        
+    }
+    
+})
