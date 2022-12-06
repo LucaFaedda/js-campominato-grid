4108aaa21574384c1f9f998c5d376e98855ef41e
LucaFaedda <lucakarl91@gmail.com>
2022-12-06 16:16:33 +0100

    creato bottom

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 
-function generateSquare(num)
+function generateSquare()
 {
-    const quadrato = document.createElement('div')
-    quadrato.classList.add('.square')
+    const quadrato = document.createElement('div') // creo un div 
+    quadrato.classList.add('square') // al div creato do la classe square
     return quadrato
 
 }
 
-const grid = document.getElementById('grind')
+const grid = document.getElementById('grind') // richiamo da html grid
 
-for (let i =0 ; i <= 100 ; i++){
+let number;
 
-    
+for (let i =0 ; i < 100 ; i++){
+
+    number = i
+    console.log(number)
+    const box = generateSquare() // qua mi sono creato i vari square
+    console.log(box)
+    grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+
 }
+
