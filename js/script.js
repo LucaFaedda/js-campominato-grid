05cbe9eb89a938f69364a73a1b3fb9461bcccb75
LucaFaedda <lucakarl91@gmail.com>
2022-12-06 17:33:09 +0100

    aggiunto img, con annessa stilizzazione css

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@ const play = document.getElementById('chek-me')
 play.addEventListener('click', function(){
     const grid = document.getElementById('grind') // richiamo da html grid
     
-    let number;
+    grid.innerHTML = ""
     
     for (let i =1 ; i <= 100 ; i++){
     
-        number = i
+        const number = i
         const box = generateSquare() // qua mi sono creato i vari square
         grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
         box.innerHTML += number
