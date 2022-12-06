c99a7a861b8edfaa167b27924528333d6ad59662
LucaFaedda <lucakarl91@gmail.com>
2022-12-06 17:38:33 +0100

    aggiunta cartella bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,32 @@
+
+
+
+function generateSquare()
+{
+    const quadrato = document.createElement('div') // creo un div 
+    quadrato.classList.add('square') // al div creato do la classe square
+    return quadrato
+}
+
+
+const play = document.getElementById('hard')
+
+play.addEventListener('click', function(){
+    const grid = document.getElementById('grind') // richiamo da html grid
+    
+    grid.innerHTML = ""
+    
+    for (let i =1 ; i <= 100 ; i++){
+    
+        const number = i
+        const box = generateSquare() // qua mi sono creato i vari square
+        grid.appendChild(box) // creo un figlio all'interno dell'elemento che vado a selezionare
+        box.innerHTML += number
+        box.addEventListener('click', function(){
+            box.classList.add('yellow')
+            console.log(number)
+        })
+        
+    }
+    
+})
