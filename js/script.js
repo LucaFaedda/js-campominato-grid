f7165da6fa76a2d6355b6758d2cbb3eda1691514
LucaFaedda <lucakarl91@gmail.com>
2022-12-06 15:22:13 +0100

    scritto funzione per creare square

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('ciao')
+
+function generateSquare(num)
+{
+    const quadrato = document.createElement('div')
+    quadrato.classList.add('.square')
+    return quadrato
+
+}
+
+const grid = document.getElementById('grind')
+
+for (let i =0 ; i <= 100 ; i++){
+
+    
+}
